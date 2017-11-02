b84b49a44746b5870f8a31d9ef4b5cc1b7fab7e0
aws-sahil <sagsahil@amazon.com>
2017-11-02 07:31:18 -0700

    Changed the code for workshop

--- CognitoHelper.cs
+++ CognitoHelper.cs
@@ -26,110 +26,29 @@ public class CognitoHelper
 
     internal async Task<bool> SignUpUser(string username, string password, string email, string phonenumber)
     {
-        AmazonCognitoIdentityProviderClient provider =
-               new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials());
-       
-        SignUpRequest signUpRequest = new SignUpRequest();
-
-        signUpRequest.ClientId= CLIENTAPP_ID;
-        signUpRequest.Username = username;
-        signUpRequest.Password=password;
-       
-
-        AttributeType attributeType = new AttributeType();
-        attributeType.Name= "phone_number";
-        attributeType.Value= phonenumber;
-        signUpRequest.UserAttributes.Add(attributeType);
-
-        AttributeType attributeType1 = new AttributeType();
-        attributeType1.Name = "email";
-        attributeType1.Value= email;
-        signUpRequest.UserAttributes.Add(attributeType1);
-
         
-        try
-        {
-
-            SignUpResponse result =await provider.SignUpAsync(signUpRequest);
-            
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return false;
-        }
         return true;
 
     }
     internal async Task<bool> VerifyAccessCode(string username, string code)
     {
-        AmazonCognitoIdentityProviderClient provider =
-               new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials());
-        ConfirmSignUpRequest confirmSignUpRequest = new ConfirmSignUpRequest();
-        confirmSignUpRequest.Username = username;
-        confirmSignUpRequest.ConfirmationCode=code;
-        confirmSignUpRequest.ClientId= CLIENTAPP_ID;
-        try
-        {
-            ConfirmSignUpResponse confirmSignUpResult = provider.ConfirmSignUp(confirmSignUpRequest);
-            Console.WriteLine(confirmSignUpResult.ToString());
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex);
-            return false;
-        }
-
         return true;
        
     }
     internal async Task<CognitoUser> ResetPassword(string username)
     {
-        AmazonCognitoIdentityProviderClient provider =
-               new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials());
-
-        CognitoUserPool userPool = new CognitoUserPool(this.POOL_ID, this.CLIENTAPP_ID, provider);
-
-        CognitoUser user = new CognitoUser(username, this.CLIENTAPP_ID, userPool, provider);
-        await user.ForgotPasswordAsync();
-        return user;
+        return null;
+       
     }
 
     internal async Task<CognitoUser> UpdatePassword(string username, string code, string newpassword)
     {
-        AmazonCognitoIdentityProviderClient provider =
-               new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials());
-
-        CognitoUserPool userPool = new CognitoUserPool(this.POOL_ID, this.CLIENTAPP_ID, provider);
-
-        CognitoUser user = new CognitoUser(username, this.CLIENTAPP_ID, userPool, provider);
-        await user.ConfirmForgotPasswordAsync(code,newpassword);
-        return user;
+        return null;
     }
 
     internal async Task<CognitoUser> ValidateUser(string username, string password) 
     {
-        AmazonCognitoIdentityProviderClient provider =
-                new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials());
-        
-        CognitoUserPool userPool = new CognitoUserPool(this.POOL_ID, this.CLIENTAPP_ID, provider);
-        
-        CognitoUser user = new CognitoUser(username, this.CLIENTAPP_ID, userPool, provider);
-        InitiateSrpAuthRequest authRequest = new InitiateSrpAuthRequest()
-        {
-            Password = password
-        };
-
-
-        AuthFlowResponse authResponse = await user.StartWithSrpAuthAsync(authRequest).ConfigureAwait(false);
-        if (authResponse.AuthenticationResult != null)
-        {
-            return user;
-        }
-        else
-        {
-            return null; 
-        }
+        return null;
     }
 
 
